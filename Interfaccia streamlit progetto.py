5348f9718db3b1a7b5cd0dbdcd0ff18aba6b4e3f
Lucabiasetto <luca.biasetto16@gmail.com>
2024-11-13 20:30:48 +0100

    pulisco

--- Interfaccia streamlit progetto.py
+++
@@ -1,6 +0,0 @@
-import polars as pl
-import streamlit as st
-import axelrod as axl
-from icecream import ic
-
-st.multiselect("scegli strategie che parteciperann al torneo",axl.axelrod_first_strategies)
