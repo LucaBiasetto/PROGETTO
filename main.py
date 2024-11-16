6734c1d38a8f30202d5a899e060551ac082d653f
Lucabiasetto <luca.biasetto16@gmail.com>
2024-11-16 19:12:47 +0100

    SIDEBAR

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from icecream import ic
 import csv
 import pandas as pd
 
+#TUTORIAL   https://www.youtube.com/watch?v=Sb0A9i6d320&list=PL7QI8ORyVSCaejt2LICRQtOTwmPiwKO2n&index=2
+
+
 #inizializzazione parametri
-players=st.multiselect("scegli strategie che parteciperann al torneo",axl.strategies,placeholder="scegli tre o più alternative: ",
+st.sidebar.header("Please filter here: ")
+players=st.sidebar.multiselect("scegli strategie che parteciperann al torneo",axl.strategies,placeholder="scegli tre o più alternative: ",
                        format_func=lambda x: str(x).strip("<'>").split('.')[-1])
 players=[p()for p in players]
 #trovare lista di strategie che mi interessa 
 #st.write(type(players[0]))
-players2=st.multiselect("scegli strategie che parteciperann al torneo",axl.strategies,placeholder="scegli due alternative: ",max_selections=2,format_func=lambda x: str(x).strip("<'>").split('.')[-1])#trovare lista di strategie che mi interessa
+players2=st.sidebar.multiselect("scegli strategie che parteciperann al torneo",axl.strategies,placeholder="scegli due alternative: ",max_selections=2,format_func=lambda x: str(x).strip("<'>").split('.')[-1])#trovare lista di strategie che mi interessa
 players2=[d()for d in players2]
-noise=st.slider("scegli il rumore : ", min_value=0.0,max_value=1.0)
-prob_end=st.slider("scegli la probabilità che il singolo match finisca ogni turno: ", min_value=0.0,max_value=1.0)
-turns=st.slider("scegli il numero di turni: ", min_value=0,max_value=1000)
+noise=st.sidebar.slider("scegli il rumore : ", min_value=0.0,max_value=1.0)
+prob_end=st.sidebar.slider("scegli la probabilità che il singolo match finisca ogni turno: ", min_value=0.0,max_value=1.0)
+turns=st.sidebar.slider("scegli il numero di turni: ", min_value=0,max_value=1000)
 
 
 #---------------Match--------------------
